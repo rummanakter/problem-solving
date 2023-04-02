e72ea890f78d11265a7379daa1a91c0fa72f6072
rummanakter <rummanakter660@gmail.com>
2023-04-03 00:55:02 +0600

    problem-6

--- main.js
+++ main.js
@@ -5,11 +5,18 @@
 // }
 
 
+
+
+
 //02-print the odd numbers less than 100
 
 // for(var i=1; i<=100; i+=2){
 //     document.write(i+"<br>");
 // }
+
+
+
+
 
 
 //03-print the multiplication table with 7
@@ -20,19 +27,58 @@
 // }
 
 
+
+
+
+
 //04-print all the multiplication tables with numbers from 1 to 10
 
-for(var i=1; i<=10; i++){
-    printTable(i);
-    console.log("");
-}
+// for(var i=1; i<=10; i++){
+//     printTable(i);
+//     console.log("");
+// }
 
-function printTable(n){
-    for(var i=1; i<=10; i++){
-        var row=n + "*" + i + "=" + n*i;
-        console.log(row); 
-    }
+// function printTable(n){
+//     for(var i=1; i<=10; i++){
+//         var row=n + "*" + i + "=" + n*i;
+//         console.log(row); 
+//     }
+// }
+
+
+
+
+
+
+//05-calculate the sum of numbers from 1 to 10
+
+// var sum=0;
+
+// for (var i=1; i<=10; i++){
+//     sum+=i;
+// }
+// console.log(sum);
+
+
+
+
+
+
+
+//06-calculate 10!
+
+var prod=1;
+
+for(var i=1; i<=10; i++){
+    prod*=i;
 }
+console.log(prod);
+
+
+
+
+
+
 
 
 
