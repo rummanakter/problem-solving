28caab95a066beb597814b72fde2640e0e7af8ca
rummanakter <rummanakter660@gmail.com>
2023-04-02 23:02:41 +0600

    problem solving 1-3

---
+++ main.js
@@ -0,0 +1,26 @@
+//01-print numbers from 1 to 10
+
+// for(var i=1; i<=10; i++){
+//     document.write(i+"<br>");
+// }
+
+
+//02-print the odd numbers less than 100
+
+// for(var i=1; i<=100; i+=2){
+//     document.write(i+"<br>");
+// }
+
+
+//03-print the multiplication table with 7
+
+for(var i=1; i<=10; i++){
+    var row="7*" + i + "=" + 7*i;
+    console.log(row);
+}
+
+
+
+
+
+
