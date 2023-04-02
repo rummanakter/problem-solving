51d79dfb7c946ec7a676ff2ad6d408829996e256
rummanakter <rummanakter660@gmail.com>
2023-04-02 23:11:34 +0600

    problem-4

--- main.js
+++ main.js
@@ -14,10 +14,26 @@
 
 //03-print the multiplication table with 7
 
+// for(var i=1; i<=10; i++){
+//     var row="7*" + i + "=" + 7*i;
+//     console.log(row);
+// }
+
+
+//04-print all the multiplication tables with numbers from 1 to 10
+
 for(var i=1; i<=10; i++){
-    var row="7*" + i + "=" + 7*i;
-    console.log(row);
+    printTable(i);
+    console.log("");
 }
+
+function printTable(n){
+    for(var i=1; i<=10; i++){
+        var row=n + "*" + i + "=" + n*i;
+        console.log(row); 
+    }
+}
+
 
 
 
